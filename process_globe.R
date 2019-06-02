885b2cb581e82f02a4b5e717fc385c0870d43eb5
kimj012 <na02093@yahoo.com>
2019-06-02 16:37:44 -0700

    hawaii data added

---
+++ process_globe.R
@@ -0,0 +1,4 @@
+library(dplyr)
+library(ggplot2)
+
+record <- read.csv(file = "record_globe.csv", stringsAsFactors = FALSE, sep = ",")
---
+++ process_hawaii.R
@@ -0,0 +1,4 @@
+library(dplyr)
+library(ggplot2)
+
+record <- read.csv(file = "record_hawaii.csv", stringsAsFactors = FALSE, sep = ",")
