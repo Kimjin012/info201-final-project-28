c91f32c9f3415587b6a9c055cf773ad0cee2bacf
Noah Joseph Tran <noahtran@uw.edu>
2019-06-03 21:55:18 -0700

    messed around with file structure, still buggy

--- climate_stats/server.R
+++ climate_stats/server.R
@@ -10,15 +10,31 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(R.utils)
 
-data <- read.csv(file = "../record_globe.csv", stringsAsFactors = FALSE, sep = ",")
-
-# Define server logic required to draw a histogram
+# Define server logic
 shinyServer(function(input, output) {
-   
+  
+  # Data set of average atmoshpiric C02 levels, collected by NOAA.
+  global_c02 <- read.csv(file = "../data/record_globe.csv", stringsAsFactors = FALSE, sep = ",")
+  # Data set of atmoshperic C02 emmisions from various sources, collected by NOAA.
+  global_c02_types <- read.csv(file = "../data/global.1751_2014.csv", stringsAsFactors = FALSE, sep = ",")
+  
+  # Render the plot of C02 based on input source.
+  output$c02_types <- renderPlot({
+    global_c02_types %>% ggplot() +
+      geom_line(aes(x = Year, y = global_c02_types[input$select])) +
+      ggtitle(paste("Atmospheric C02 Levels From ", input$select)) +
+      xlab("Year") + 
+      ylab("Metric Tons of Atmoshperici C02")
+  })
+  
+  # Render the plot of average atmosheric C02 levels.
   output$c02_plot <- renderPlot({
-    data %>% filter(average >= 0) %>% ggplot() +
-      geom_line(aes(x = decimal, y = average)) +
+    global_c02 %>% filter(average >= 0) %>% ggplot() + #Removes negative values, which represent missing data.
+      geom_line(aes(x = decimal, y = average, color = "C02")) +
+      geom_line(aes(x = decimal, y = trend, color = "Trend")) +
+      ggtitle("Average Atmoshperic C02 Levels") +
       xlab("Year") + 
       ylab("C02 parts per Million")
   })
--- climate_stats/ui.R
+++ climate_stats/ui.R
@@ -10,6 +10,9 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(R.utils)
+
+c02_types <- read.csv(file = "../data/global.1751_2014.csv", stringsAsFactors = FALSE, sep = ",")
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -20,10 +23,15 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
+      # User Input for slelecting a Atmosphiric C02 Emmision Type
+      selectInput("select", label = h3("Select Emmision Type"), 
+                  choices = colnames(c02_types)[-1], 
+                  selected = 1)
     ), 
     
     # Show a plot of the generated distribution
     mainPanel(
+      plotOutput("c02_types"),
       plotOutput("c02_plot")
     )
   )
