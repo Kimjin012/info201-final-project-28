5de6635664845ef99ade9c993f0c793856e73b42
Marc Antonio <MarcJoshuaAntonioBusiness@gmail.com>
2019-06-05 00:23:42 -0800

    Added the ability to hide co2 emission sources

--- climate_stats/server.R
+++ climate_stats/server.R
@@ -16,9 +16,9 @@ library(R.utils)
 shinyServer(function(input, output) {
   
   # Data set of average atmoshpiric C02 levels, collected by NOAA.
-  global_c02 <- read.csv(file = "../data/record_globe.csv", stringsAsFactors = FALSE, sep = ",")
+  global_c02 <- read.csv(file = "data/record_globe.csv", stringsAsFactors = FALSE, sep = ",")
   # Data set of atmoshperic C02 emmisions from various sources, collected by NOAA.
-  global_c02_types <- read.csv(file = "../data/global.1751_2014.csv", stringsAsFactors = FALSE, sep = ",")
+  global_c02_types <- read.csv(file = "data/global.1751_2014.csv", stringsAsFactors = FALSE, sep = ",")
   
   output$message <- reactive({
     paste(input$range[1])
@@ -26,27 +26,48 @@ shinyServer(function(input, output) {
   
   # Render the plot of C02 based on input source.
   output$c02_types <- renderPlot({
-    global_c02_types %>% filter(Year >= input$range[1], Year <= input$range[2]) %>%  ggplot() +
-      geom_line(aes(x = Year, y = Total, color = "Total")) +
-      geom_line(aes(x = Year, y = Gas_Fuel, color = "Gas Fuel")) +
-      geom_line(aes(x = Year, y = Liquid_Fuel, color = "Liquid Fuel")) +
-      geom_line(aes(x = Year, y = Solid_Fuel, color = "Solid Fuel")) +
-      geom_line(aes(x = Year, y = Cement_Production, color = "Cement Production")) +
-      geom_line(aes(x = Year, y = Gas_Flairing, color = "Gas Flaring")) +
+    global_c02_types %>% filter(Year >= input$range[1], Year <= input$range[2])
+    #if(1 %in% input$co_2_type) {
+     # global_c02_types <- 
+    #}
+    for(column_name in input$co_2_type) {
+      global_c02_types <- select(global_c02_types, -c(UQ(column_name)))
+    }
+      temp <- ggplot(global_c02_types)
+      temp <- temp + geom_line(aes(x = Year, y = Total, color = "Total"))
+      if("Gas_Fuel" %in% colnames(global_c02_types))  {
+        temp <- temp + geom_line(aes(x = Year, y = Gas_Fuel, color = "Gas Fuel"))
+      }
+      if("Liquid_Fuel" %in% colnames(global_c02_types))  {
+        temp <- temp + geom_line(aes(x = Year, y = Liquid_Fuel, color = "Liquid Fuel")) 
+      }
+      if("Solid_Fuel" %in% colnames(global_c02_types))  {
+        temp <- temp + geom_line(aes(x = Year, y = Solid_Fuel, color = "Solid Fuel")) 
+      }
+      if("Cement_Production" %in% colnames(global_c02_types))  {
+        temp <- temp + geom_line(aes(x = Year, y = Cement_Production, color = "Cement Production")) 
+      }
+      if("Gas_Flairing" %in% colnames(global_c02_types))  {
+        temp <- temp + geom_line(aes(x = Year, y = Gas_Flairing, color = "Gas Flairing")) 
+      }
+      temp <- temp +
       geom_line(aes(x = Year, y = Per_Capita, color = "Per Capita (metric tons)")) +
       ggtitle(paste("Atmospheric C02 Levels")) +
       xlab("Year") + 
       ylab("Million Metric Tons of Atmoshperici C02")
+      return(temp)
   })
   
   # Render the plot of average atmosheric C02 levels.
   output$c02_plot <- renderPlot({
-    global_c02 %>% filter(average >= 0) %>% filter(year >= input$range[1], year <= input$range[2]) %>% ggplot() + #Removes negative values, which represent missing data.
+    global_c02 %>% filter(average >= 0) %>% filter(year >= input$range[1], year <= input$range[2]) %>% 
+      ggplot() + #Removes negative values, which represent missing data.
       geom_line(aes(x = decimal, y = average, color = "C02")) +
       geom_line(aes(x = decimal, y = trend, color = "Trend")) +
       ggtitle("Average Atmoshperic C02 Levels") +
       xlab("Year") + 
       ylab("C02 parts per Million")
   })
+  
   
 })
